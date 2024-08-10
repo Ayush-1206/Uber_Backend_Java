2f6722bc196c799d0dec9c83c30e6aa99e66819f
Ayush-1206 <ayushbankhede12@gmail.com>
2024-08-11 01:03:47 +0530

    Review table entity(Modal)

--- UberReviewService/src/main/java/com/example/UberReviewService/models/Review.java
+++ UberReviewService/src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,13 +1,30 @@
 package com.example.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+//table(name = "bookingreview")
 public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     Long id;
+
+    @Column(nullable = false)
+    String Content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 }
